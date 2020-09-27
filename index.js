c47bcfca0ccabe02ad3b9c0f12024f50fbf0273d
Chris <raoxc@mogulinker.com>
2020-09-27 21:28:03 +0800

    初始化
    npm link 链接当前项目到全局node_modules下生成快捷方式，全局访问

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/*
+ * @Author: Chris
+ * @Date: 2020-09-27 21:23:48
+ * @LastEditors: Chris
+ * @LastEditTime: 2020-09-27 21:26:16
+ * @Descripttion: **
+ */
+console.log('taro-wx-cli');
